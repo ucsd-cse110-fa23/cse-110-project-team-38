49168805e3c161d6d7fe679eb328161152ac3066
ayeUCSD <114363008+ayeUCSD@users.noreply.github.com>
2023-11-06 16:59:55 -0800

    created template test in pantrypal src

---
+++ src/PantryPal/TestTemplate.java
@@ -0,0 +1,23 @@
+package PantryPal;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.testfx.framework.junit.ApplicationTest;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestTemplate {
+    @Test
+    public void exampleTest(){
+        assertEquals(true,1 == 1);
+    }
+
+    @Test
+    public void terribleTest(){
+        assertEquals(true,false);
+    }
+}
